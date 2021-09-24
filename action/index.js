579a96872c786f63165a8abf7453bde25d32aabb
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-24 20:27:26 -0300

    refacotr: action

--- action/index.js
+++ action/index.js
@@ -620,6 +620,7 @@ const logger_1 = __nccwpck_require__(231);
     const logger = new logger_1.Logger();
     // const actionHelper = new ActionHelper(logger)
     logger.debug('hello tcha tcha tcha!');
+    console.log('hello tcha tcha tcha!');
     // const args = actionHelper.getActionArgs()
     // if (args.right) {
     //   logger.debug(args.right.flags.join(','))
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Logger } from './logger'
   const logger = new Logger()
   // const actionHelper = new ActionHelper(logger)
   logger.debug('hello tcha tcha tcha!')
+  console.log('hello tcha tcha tcha!')
   // const args = actionHelper.getActionArgs()
   // if (args.right) {
   //   logger.debug(args.right.flags.join(','))
