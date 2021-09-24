a795da7f61e27270da41818078ec4979b169cd36
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-24 20:30:04 -0300

    refacotr: action

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-// import { ActionHelper } from './action_helper'
+import { ActionHelper } from './action_helper'
 import { Logger } from './logger'
 
 ;(() => {
   const logger = new Logger()
-  // const actionHelper = new ActionHelper(logger)
+  const actionHelper = new ActionHelper(logger)
   logger.debug('hello tcha tcha tcha!')
   console.log('hello tcha tcha tcha!')
-  // const args = actionHelper.getActionArgs()
-  // if (args.right) {
-  //   logger.debug(args.right.flags.join(','))
-  //   logger.debug(JSON.stringify(args.right.args))
-  // }
+  const args = actionHelper.getActionArgs()
+  if (args.right) {
+    console.log(args.right.flags.join(','))
+    console.log(JSON.stringify(args.right.args))
+  }
 })()
