1cbc80da26acec937a103f4869302592ca0afda6
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-24 18:05:23 -0300

    refactor: beta action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { ActionHelper } from './action_helper'
+
 import { Logger } from './logger'
 
 ;(() => {
   const logger = new Logger()
-  const actionHelpper = new ActionHelper(logger)
-  logger.debug('hello tcha tcha tcha!')
-  logger.debug(actionHelpper.getArguments().join(','))
+  logger.info('hello tcha tcha tcha!')
 })()
