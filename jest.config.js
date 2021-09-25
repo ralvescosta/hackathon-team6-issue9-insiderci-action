e524e50583e73d23a95e97893570dfe25b13ae16
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-25 15:29:51 -0300

    tests: coverage

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      branches: 92,
+      functions: 100,
+      lines: 100,
+      statements: 100
     }
   },
   testEnvironment: 'node',
--- test/insiderci_installer.spec.ts
+++ test/insiderci_installer.spec.ts
@@ -1,11 +1,123 @@
+import { InsiderCiInstaller } from '../src/insiderci_installer'
+import { loggerSpy } from './mocks'
+import { ICache, IHttpClient } from '../src/interfaces'
+import * as os from 'os'
+
+jest.mock('os', () => ({
+  platform: jest.fn(() => 'linux'),
+  arch: jest.fn(() => 'x64')
+}))
+
+const makeSut = () => {
+  const httpClientSpy: IHttpClient = {
+    get: jest.fn()
+  }
+  const cacheSpy: ICache = {
+    getTool: jest.fn(),
+    extract: jest.fn(),
+    cache: jest.fn()
+  }
+  const sut = new InsiderCiInstaller(httpClientSpy, cacheSpy, loggerSpy)
+
+  return {
+    sut,
+    httpClientSpy,
+    cacheSpy
+  }
+}
 describe('#InsiderCiInstaller', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
   describe('#exec', () => {
-    it('', () => {
-      expect(true).toBeTruthy()
+    it('should return insider ci executable path when execute correctly in linux environment', async () => {
+      const { sut, httpClientSpy, cacheSpy } = makeSut()
+      jest.spyOn(httpClientSpy, 'get').mockResolvedValueOnce({ right: { tag_name: 'tag' } })
+      jest.spyOn(cacheSpy, 'getTool').mockResolvedValueOnce({ right: 'tool_path' })
+      jest.spyOn(cacheSpy, 'extract').mockResolvedValueOnce({ right: 'extracted_path' })
+      jest.spyOn(cacheSpy, 'cache').mockResolvedValueOnce({ right: 'cached_path' })
+
+      const result = await sut.exec('version')
+
+      expect(result.left).toBeUndefined()
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.getTool).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.extract).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.cache).toHaveBeenCalledTimes(1)
+    })
+    it('should return insider ci executable path when execute correctly in windows environment', async () => {
+      const { sut, httpClientSpy, cacheSpy } = makeSut()
+      jest.spyOn(os, 'platform').mockReturnValueOnce('win32')
+      jest.spyOn(os, 'arch').mockReturnValueOnce('i386')
+      jest.spyOn(httpClientSpy, 'get').mockResolvedValueOnce({ right: { tag_name: 'tag' } })
+      jest.spyOn(cacheSpy, 'getTool').mockResolvedValueOnce({ right: 'tool_path' })
+      jest.spyOn(cacheSpy, 'extract').mockResolvedValueOnce({ right: 'extracted_path' })
+      jest.spyOn(cacheSpy, 'cache').mockResolvedValueOnce({ right: 'cached_path' })
+
+      const result = await sut.exec('version')
+
+      expect(result.left).toBeUndefined()
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.getTool).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.extract).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.cache).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return error if #httpClient.get returns error', async () => {
+      const { sut, httpClientSpy } = makeSut()
+      jest.spyOn(httpClientSpy, 'get').mockResolvedValueOnce({ left: new Error('') })
+
+      const result = await sut.exec('version')
+
+      expect(result.right).toBeUndefined()
+      expect(result.left).toBeInstanceOf(Error)
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return error if #cache.getTool returns error', async () => {
+      const { sut, httpClientSpy, cacheSpy } = makeSut()
+      jest.spyOn(httpClientSpy, 'get').mockResolvedValueOnce({ right: { tag_name: 'tag' } })
+      jest.spyOn(cacheSpy, 'getTool').mockResolvedValueOnce({ left: new Error('') })
+
+      const result = await sut.exec('version')
+
+      expect(result.right).toBeUndefined()
+      expect(result.left).toBeInstanceOf(Error)
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.getTool).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return error if #cache.extract returns error', async () => {
+      const { sut, httpClientSpy, cacheSpy } = makeSut()
+      jest.spyOn(httpClientSpy, 'get').mockResolvedValueOnce({ right: { tag_name: 'tag' } })
+      jest.spyOn(cacheSpy, 'getTool').mockResolvedValueOnce({ right: 'tool_path' })
+      jest.spyOn(cacheSpy, 'extract').mockResolvedValueOnce({ left: new Error('') })
+
+      const result = await sut.exec('version')
+
+      expect(result.right).toBeUndefined()
+      expect(result.left).toBeInstanceOf(Error)
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.getTool).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.extract).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return error if #cache.cache returns error', async () => {
+      const { sut, httpClientSpy, cacheSpy } = makeSut()
+      jest.spyOn(httpClientSpy, 'get').mockResolvedValueOnce({ right: { tag_name: 'tag' } })
+      jest.spyOn(cacheSpy, 'getTool').mockResolvedValueOnce({ right: 'tool_path' })
+      jest.spyOn(cacheSpy, 'extract').mockResolvedValueOnce({ right: 'extracted_path' })
+      jest.spyOn(cacheSpy, 'cache').mockResolvedValueOnce({ left: new Error('') })
+
+      const result = await sut.exec('version')
+
+      expect(result.right).toBeUndefined()
+      expect(result.left).toBeInstanceOf(Error)
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.getTool).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.extract).toHaveBeenCalledTimes(1)
+      expect(cacheSpy.cache).toHaveBeenCalledTimes(1)
     })
   })
 })
